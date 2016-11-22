926b52f698c2988874ce705e5db4b0bbf5802230
Viktória Tibenská <vtibensk8@gmail.com>
2016-11-22 22:51:11 +0100

    list of patterns added

--- app/app.component.js
+++ app/app.component.js
@@ -9,6 +9,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var PATTERNS = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Browse patterns';
@@ -16,11 +28,13 @@ var AppComponent = (function () {
             id: 1,
             name: 'Architect also implements'
         };
+        this.patterns = PATTERNS;
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n        <h1>{{title}}</h1>\n        <h2>{{pattern.name}} details!</h2>\n        <div><label>id: </label>{{pattern.id}}</div>\n        <div>\n            <label>name: </label>\n            <input [(ngModel)]=\"pattern.name\" placeholder=\"name\">\n        </div>\n      "
+            template: "\n        <h1>{{title}}</h1>\n\n        <h2>Patterns</h2>\n        <ul class=\"patterns\">\n        <li *ngFor=\"let pattern of patterns\">\n            <span class=\"badge\">{{pattern.id}}</span> {{pattern.name}}\n        </li>\n        </ul>\n\n        <h2>{{pattern.name}} details!</h2>\n        <div><label>id: </label>{{pattern.id}}</div>\n        <div>\n            <label>name: </label>\n            <input [(ngModel)]=\"pattern.name\" placeholder=\"name\">\n        </div>\n      ",
+            styles: ["\n        .selected {\n            background-color: #CFD8DC !important;\n            color: white;\n        }\n        .patterns {\n            margin: 0 0 2em 0;\n            list-style-type: none;\n            padding: 0;\n            width: 15em;\n        }\n        .patterns li {\n            cursor: pointer;\n            position: relative;\n            left: 0;\n            background-color: #EEE;\n            margin: .5em;\n            padding: .3em 0;\n            height: 1.6em;\n            border-radius: 4px;\n        }\n        .patterns li.selected:hover {\n            background-color: #BBD8DC !important;\n            color: white;\n        }\n        .patterns li:hover {\n            color: #607D8B;\n            background-color: #DDD;\n            left: .1em;\n        }\n        .patterns .text {\n            position: relative;\n            top: -3px;\n        }\n        .patterns .badge {\n            display: inline-block;\n            font-size: small;\n            color: white;\n            padding: 0.8em 0.7em 0 0.7em;\n            background-color: #607D8B;\n            line-height: 1em;\n            position: relative;\n            left: -1px;\n            top: -4px;\n            height: 1.8em;\n            margin-right: .8em;\n            border-radius: 4px 0 0 4px;\n        }\n    "]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,86 @@
 import { Component } from '@angular/core';
 
+const PATTERNS: Pattern[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
 @Component({
     selector: 'my-app',
     template:`
         <h1>{{title}}</h1>
+
+        <h2>Patterns</h2>
+        <ul class="patterns">
+        <li *ngFor="let pattern of patterns">
+            <span class="badge">{{pattern.id}}</span> {{pattern.name}}
+        </li>
+        </ul>
+
         <h2>{{pattern.name}} details!</h2>
         <div><label>id: </label>{{pattern.id}}</div>
         <div>
             <label>name: </label>
             <input [(ngModel)]="pattern.name" placeholder="name">
         </div>
-      `
+      `,
+    styles: [`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+        }
+        .patterns {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 15em;
+        }
+        .patterns li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: .3em 0;
+            height: 1.6em;
+            border-radius: 4px;
+        }
+        .patterns li.selected:hover {
+            background-color: #BBD8DC !important;
+            color: white;
+        }
+        .patterns li:hover {
+            color: #607D8B;
+            background-color: #DDD;
+            left: .1em;
+        }
+        .patterns .text {
+            position: relative;
+            top: -3px;
+        }
+        .patterns .badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0 0 4px;
+        }
+    `]
 })
 
 export class AppComponent {
@@ -18,7 +88,8 @@ export class AppComponent {
     pattern: Pattern = {
         id: 1,
         name: 'Architect also implements'
-    }
+    };
+    patterns = PATTERNS;
 }
 
 export class Pattern {
