f9e3052ff701c4e4f04869fd75f2e39a2b54a908
Viktória Tibenská <vtibensk8@gmail.com>
2016-11-22 23:19:32 +0100

    detail selected from list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,37 +1,20 @@
 import { Component } from '@angular/core';
 
 const PATTERNS: Pattern[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Divide and conquer' },
+    { id: 12, name: 'Organization follows location' },
+    { id: 13, name: 'Face to face before working remotely' },
+    { id: 14, name: 'Architect controls product' },
+    { id: 15, name: 'Stand up meeting' },
+    { id: 16, name: 'Deploy along the grain' },
+    { id: 17, name: 'Architect also implements' },
+    { id: 18, name: 'Standards linking locations' },
+    { id: 19, name: 'Code ownership' },
+    { id: 20, name: 'Variation behind interface' }
 ];
 
 @Component({
     selector: 'my-app',
-    template:`
-        <h1>{{title}}</h1>
-
-        <h2>Patterns</h2>
-        <ul class="patterns">
-        <li *ngFor="let pattern of patterns">
-            <span class="badge">{{pattern.id}}</span> {{pattern.name}}
-        </li>
-        </ul>
-
-        <h2>{{pattern.name}} details!</h2>
-        <div><label>id: </label>{{pattern.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="pattern.name" placeholder="name">
-        </div>
-      `,
     styles: [`
         .selected {
             background-color: #CFD8DC !important;
@@ -80,16 +63,36 @@ const PATTERNS: Pattern[] = [
             margin-right: .8em;
             border-radius: 4px 0 0 4px;
         }
-    `]
+    `],
+    template:`
+        <h1>{{title}}</h1>
+
+        <h2>Patterns</h2>
+        <ul class="patterns">
+        <li *ngFor="let pattern of patterns" [class.selected]="pattern === selectedPattern" (click)="onSelect(pattern)">
+            <span class="badge">{{pattern.id}}</span> {{pattern.name}}
+        </li>
+        </ul>
+
+        <div *ngIf="selectedPattern">
+            <h2>{{selectedPattern.name}} details!</h2>
+            <div><label>id: </label>{{selectedPattern.id}}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedPattern.name" placeholder="name">
+            </div>
+        </div>
+      `
 })
 
 export class AppComponent {
     title = 'Browse patterns';
-    pattern: Pattern = {
-        id: 1,
-        name: 'Architect also implements'
-    };
+    selectedPattern: Pattern;
     patterns = PATTERNS;
+
+    onSelect(pattern: Pattern): void {
+        this.selectedPattern = pattern;
+    }
 }
 
 export class Pattern {
